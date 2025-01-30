b276cfca325646a54058b95bf145c962be318743
Adityakirthivasan <adityakirthivasan@gmail.com>
2025-01-30 08:26:07 +0530

    first

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -12,7 +12,7 @@ const Contact = () => {
     e.preventDefault();
     await axios
       .post(
-        "http://localhost:4000/api/v1/message/send",
+        "https://project3-backend-udfy.onrender.com/api/v1/message/send",
         {
           name,
           email,
